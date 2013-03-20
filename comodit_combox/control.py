2f059f12533691a81f9d4cf82e5343a58504c872
Laurent Eschenauer <laurent@eschenauer.be>
2013-03-20 16:16:15 +0100

    Added start/stop

---
+++ comodit_combox/control.py
@@ -0,0 +1,16 @@
+import time, sys, os, helper
+
+from comodit_client.api import Client
+from comodit_client.api.exceptions import PythonApiException
+from comodit_client.rest.exceptions import ApiException
+
+from helper import create_host, get_short_hostname, exec_cmd, exec_cmds, fork_cmd
+
+def stop(config):
+    print "Stopping virtual machine"
+    success = exec_cmds(['VBoxManage controlvm "%s" poweroff' % config['vm']['name']])
+
+def start(config):
+    print "Starting virtual machine"
+    fork_cmd('virtualbox --startvm "%s"' % config['vm']['name'])
+
